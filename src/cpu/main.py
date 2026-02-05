c8550dfcdd07d7cf365911a9a7586bcc3bb95917
claudiocamolese <claudiocamolese@gmail.com>
2026-02-05 16:10:14 +0100

    main changed pin memory

--- src/cpu/main.py
+++ src/cpu/main.py
@@ -1,6 +1,9 @@
 import time
 import torch
 import argparse
+import os
+import random
+
 from torchvision import datasets, transforms
 
 from model import Net
@@ -8,7 +11,9 @@ from train import Trainer
 
 
 def main(args):
+    random.seed(21)
     device = args.device
+    num_workers = max(1, os.cpu_count() // 2)
 
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
@@ -17,24 +22,15 @@ def main(args):
             download=True,
             transform=transforms.ToTensor()
         ),
-        batch_size=64,      # aumenta batch size per sfruttare meglio la GPU
-        #shuffle=True,
-        #num_workers=4,       # dipende dal numero di core CPU
-        #pin_memory=True      # trasferimento più veloce verso GPU
-    )
+        batch_size= 64,      
+        shuffle= True,
+        num_workers= num_workers,       
+        pin_memory= True)     
 
-    trainer = Trainer(
-        epochs=5,
-        lr=1e-2,    
-        train_loader=train_loader,
-        device=device
-    )
+    trainer = Trainer(epochs=5, lr=1e-2, train_loader= train_loader, device= device)
 
     model = Net()
 
-    # -------------------
-    # TIMING START
-    # -------------------
     if device == "cuda":
         torch.cuda.synchronize()
     start_time = time.perf_counter()
@@ -44,9 +40,6 @@ def main(args):
     if device == "cuda":
         torch.cuda.synchronize()
     end_time = time.perf_counter()
-    # -------------------
-    # TIMING END
-    # -------------------
 
     print(f"Training time: {end_time - start_time:.3f} seconds")
 
