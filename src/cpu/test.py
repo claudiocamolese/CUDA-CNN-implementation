46f8251fd5c3431eb862d9bf7ec5eda48a400cad
claudiocamolese <claudiocamolese@gmail.com>
2026-02-05 17:23:59 +0100

    criterion

--- src/cpu/test.py
+++ src/cpu/test.py
@@ -1,13 +1,13 @@
-import torch
-
 from tqdm import tqdm
+import torch
 from torch.nn import CrossEntropyLoss
 
+
 class Tester:
-    def __init__(self, test_loader, device):
+    def __init__(self, test_loader, device, criterion=None):
         self.test_loader = test_loader
         self.device = device
-        self.criterion = CrossEntropyLoss
+        self.criterion = criterion if criterion is not None else CrossEntropyLoss()
 
     @torch.no_grad()
     def test(self, model):
@@ -19,6 +19,9 @@ class Tester:
 
         loop = tqdm(self.test_loader, desc="Testing")
 
+        all_preds = []
+        all_labels = []
+
         for img, label in loop:
             img = img.to(self.device)
             label = label.to(self.device)
@@ -26,12 +29,21 @@ class Tester:
             pred = model(img)
 
             if self.criterion is not None:
+
+                if isinstance(self.criterion, CrossEntropyLoss):
+                    label = label.view(-1).long()
+
                 loss = self.criterion(pred, label)
                 running_loss += loss.item()
                 loop.set_postfix(loss=running_loss / (loop.n + 1))
 
-        if self.criterion is not None:
-            avg_loss = running_loss / num_batches
-            print(f"Test avg loss: {avg_loss:.6f}")
-            return avg_loss
+            all_preds.append(pred.cpu())
+            all_labels.append(label.cpu())
 
+        avg_loss = running_loss / num_batches if self.criterion is not None else None
+        if avg_loss is not None:
+            print(f"Test avg loss: {avg_loss:.6f}")
+
+        all_preds = torch.cat(all_preds)
+        all_labels = torch.cat(all_labels)
+        return avg_loss, all_preds, all_labels
