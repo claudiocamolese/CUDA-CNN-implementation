0273a4a6b56596c299c9e0f6b9d2463227619358
claudiocamolese <claudiocamolese@gmail.com>
2026-02-07 18:44:58 +0100

    printing.h

--- src/utils/printing.h
+++ src/utils/printing.h
@@ -1,4 +1,4 @@
 #pragma once
 
 void print_progress(int current, int total, int epoch, int epochs);
-void print_epoch_end(int epoch, int epochs, float avg_loss);
+void print_epoch_end(float avg_loss);
